197720984433b2057c2e44daebf2838a8c53bbfa
Scott McAllister <scottm@nextopia.com>
2015-09-13 19:59:27 -0400

    fixed interface and added Tastekid API interaction

--- showlike/urls.py
+++ showlike/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
 	url(r'^$', views.home, name='home'),
-
+	url(r'^search', views.search, name='search'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- showlike/views.py
+++ showlike/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.views import generic
 
+import urllib
+import urllib2
+import json
+
 def home(request):
 	context = {'test': 'test'}
 	return render(request, 'index.html', context)
+
+def search(request):
+	query = request.GET['query']
+	query = urllib.unquote(query.encode('ascii')).decode('utf-8')
+	query = urllib.quote(query.encode('utf-8'))
+	stuff = urllib2.urlopen('https://www.tastekid.com/api/similar?q='+query+'&k=159747-ShowLike-M27GAIEQ&verbose=1')
+	decoded = json.load(stuff)
+	context = {	
+				'keywords': request.GET['query'],
+				'info' : decoded['Similar']['Info'],
+				'results': decoded['Similar']['Results']	
+			}
+
+	return render(request, 'search.html', context)
