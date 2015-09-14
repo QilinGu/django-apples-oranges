ea28702a8ab2a2fdc42648747cc418ea77ecc118
Scott McAllister <scottm@nextopia.com>
2015-09-14 08:54:03 -0400

    change tastekid API call to use http

--- showlike/views.py
+++ showlike/views.py
@@ -13,7 +13,7 @@ def search(request):
 	query = request.GET['query']
 	query = urllib.unquote(query.encode('ascii')).decode('utf-8')
 	query = urllib.quote(query.encode('utf-8'))
-	stuff = urllib2.urlopen('https://www.tastekid.com/api/similar?q='+query+'&k=159747-ShowLike-M27GAIEQ&verbose=1')
+	stuff = urllib2.urlopen('http://www.tastekid.com/api/similar?q='+query+'&k=159747-ShowLike-M27GAIEQ&verbose=1')
 	decoded = json.load(stuff)
 	context = {	
 				'keywords': request.GET['query'],
